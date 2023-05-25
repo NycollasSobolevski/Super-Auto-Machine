19d36699f3c63221ca1143c3d820b682ed813dea
Nycollas <sobolevski.academy@gmail.com>
2023-05-25 11:44:53 -0300

    ADD: Args (para os especiais da Machine )

---
+++ Super Auto Machine/Args.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Super_Auto_Machine;
+
+public class ProcessArgs
+{
+    private static ProcessArgs empty = new();
+    public static ProcessArgs Empty => empty;
+}
+
+public class BattleArgs : ProcessArgs
+{
+    public Store Store { get; set; }
+    public Machine Machine { get; set; }
+}
+
+public class StoreArgs : ProcessArgs
+{
+    public Player Player { get; set; }
+    public Machine Machine { get; set; }
+}
