f3fc54884363501e013be78c1d9e93747cae99b9
Nycollas <sobolevski.academy@gmail.com>
2023-05-25 08:53:15 -0300

    UPDATE

--- Super Auto Machine/Machines.cs
+++ Super Auto Machine/Machines.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Super_Auto_Machine
+namespace Super_Auto_Machine;
+public abstract class Machines
 {
-    internal class Mahcines
+    public abstract int Life { get; set; }
+    public abstract int Level { get; set; }
+    public abstract int Exp { get; set; }
+    public abstract int Tier { get; set; }
+    public abstract int Power { get; set; }
+    public abstract void Atack();
+}
+
+public interface IBuilder
+{
+    
+}
+
+
+public class Hammer : Machines
+{
+    public override int Life { get; set; } = 3;
+    public override int Power { get; set; } = 2;
+    public override int Level { get; set ; }
+    public override int Exp { get ; set; }
+    public override int Tier { get; set ; }
+
+    public override void Atack()
     {
+        throw new NotImplementedException();
     }
 }
